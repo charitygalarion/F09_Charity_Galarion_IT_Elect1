3dc55a41cd049b035aa78e894d84538edd8f5878
charitygalarion <charitygalarion3@gmail.com>
2025-09-24 22:34:48 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import CounterApp from './CounterApp';
+
+
+import {SafeAreaView,StyleSheet} from 'react-native';
+import ColorChangerApp from './ColorChangerApp.js';
+import CounterApp from "./CounterApp.js";
+export default function App() {
+
+  return (
+    
+ <SafeAreaView style={styles.container}>
+<CounterApp/>
+ <ColorChangerApp />
+ </SafeAreaView>
+   )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+});
